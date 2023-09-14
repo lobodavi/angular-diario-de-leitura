d3ab219d05986499fe4da3ac8e5f141968ceed5d
lobodavi <lobodavi@hotmail.com>
2023-09-14 12:16:24 -0300

    criação do projeto

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,9 @@
+export const dataFake = [
+    {
+    "id":"1",
+    "photo":"https://m.media-amazon.com/images/I/41J3FApF8yL.jpg",
+    "title":"Titulo",
+    "Author": "Alex michealis",
+    "description":"Autor: Alex Michaelis <br> \n Ano:  /n Numero de páginas:  Editora:"
+    }
+]
